faca6edb6148a298f7806d29539e0c853a5ed243
Wight554 <wight554@gmail.com>
2018-02-18 20:33:59 +0000

    ABCOTA: Add adaptive icon

--- app/src/main/java/com/abc/ota/tasks/CheckUpdateTask.java
+++ app/src/main/java/com/abc/ota/tasks/CheckUpdateTask.java
@@ -167,7 +167,7 @@ public class CheckUpdateTask extends AsyncTask<Context, Void, OTADevice> {
             notificationManager.createNotificationChannel(mChannel);
 
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context.getApplicationContext())
-                    .setSmallIcon(R.drawable.ic_notification_abc)
+                    .setSmallIcon(R.mipmap.ic_launcher_abc)
                     .setContentTitle(context.getString(R.string.notification_title))
                     .setContentText(context.getString(R.string.notification_message))
                     .setOnlyAlertOnce(true)
